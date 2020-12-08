df91d269a6d3edec598d9fd70bfb54cf008eebaf
Katsute <58778985+Katsute@users.noreply.github.com>
2020-12-07 22:49:45 -0500

    Added forum tests

--- src/main/java/com/kttdevelopment/myanimelist/PaginatedIterator.java
+++ src/main/java/com/kttdevelopment/myanimelist/PaginatedIterator.java
@@ -2,6 +2,15 @@ package com.kttdevelopment.myanimelist;
 
 import java.util.*;
 
+/**
+ * Represents a paged response.
+ *
+ * @param <T> type
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author Ktt Development
+ */
 public abstract class PaginatedIterator<T> implements Iterator<T> {
 
     private int index = 0; // thread safe by methods
@@ -27,6 +36,13 @@ public abstract class PaginatedIterator<T> implements Iterator<T> {
             throw new NoSuchElementException();
     }
 
+    /**
+     * Returns if the response has a next page.
+     *
+     * @return if next page
+     *
+     * @since 1.0.0
+     */
     public abstract boolean hasNextPage();
 
     public synchronized final void nextPage(){
@@ -37,6 +53,13 @@ public abstract class PaginatedIterator<T> implements Iterator<T> {
             throw new NoSuchElementException();
     }
 
+    /**
+     * Returns the next page in the response.
+     *
+     * @return next page
+     *
+     * @since 1.0.0
+     */
     protected abstract List<T> getNextPage();
 
     @Override
@@ -44,10 +67,24 @@ public abstract class PaginatedIterator<T> implements Iterator<T> {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Returns the current page as a list.
+     *
+     * @return list
+     *
+     * @since 1.0.0
+     */
     public final List<T> toList(){
         return Collections.unmodifiableList(list);
     }
 
+    /**
+     * Returns the current page as a set.
+     *
+     * @return set
+     *
+     * @since 1.0.0
+     */
     public final Set<T> toSet(){
         return Collections.unmodifiableSet(new LinkedHashSet<>(list));
     }
