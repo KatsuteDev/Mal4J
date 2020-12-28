ea7696b69817f2a37857652458185d317e862e6f
Katsute <58778985+Katsute@users.noreply.github.com>
2020-12-28 14:34:49 -0500

    api call

--- src/main/java/com/kttdevelopment/myanimelist/APICall.java
+++ src/main/java/com/kttdevelopment/myanimelist/APICall.java
@@ -1,67 +1,158 @@
 package com.kttdevelopment.myanimelist;
 
-import java.lang.reflect.*;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-import static com.kttdevelopment.myanimelist.APIReader.*;
+final class APICall {
 
-class APICall implements InvocationHandler {
-
+    private final String method;
     private final String baseURL;
 
-    public APICall(final String baseURL){
+    APICall(final String method, final String baseURL){
+        this.method = method;
         this.baseURL = baseURL;
     }
 
-    @Override
-    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable{
-        // fixme
-        // if(method.getDeclaringClass() != service)
-        //     return method.invoke(this, args);
-
-        final Class<?> rtn = method.getReturnType();
-        final Object[] params = args != null ? args : new Object[0];
-
-        final ENDPOINT endpoint;
-        if((endpoint = method.getAnnotation(ENDPOINT.class)) != null){
-            final String rmethod = endpoint.method();
-
-
-
-            final Map<String,String> pathParams = new HashMap<>();
-            for(final Parameter parameter : method.getParameters()){
-                for(final Path apath : parameter.getAnnotationsByType(Path.class)){
-                    pathParams.put(apath.value(), null); // fixme param value
-                }
-            }
-
-            final String URL = baseURL + constructPath(endpoint.value(), null); // todo: query ?&
-
-            // todo HTTP request
-
-            return Json.parse(null);
-        }else{
-            return method.invoke(this, args);
-        }
-    }
+    private final Map<String,String> headers = new HashMap<>();
 
     // \{(.*?)\}
-    private static final Pattern path = Pattern.compile("\\{(.*?)}");
+    private static final Pattern pathArg = Pattern.compile("\\{(.*?)}");
 
-    private static String constructPath(final String path, final Map<String,String> pathValues){
-        return APICall.path.matcher(path).replaceAll(matchResult -> pathValues.getOrDefault(matchResult.group(), ""));
+    private String path = "";
+    private final Map<String,String> pathVars = new HashMap<>();
+    private final Map<String,String> queries  = new HashMap<>();
+
+    private boolean formUrlEncoded = false;
+    private final Map<String,String> fields = new HashMap<>();
+
+    final APICall withHeader(final String header, final String value){
+        if(value == null)
+            headers.remove(header);
+        else
+            headers.put(header, value);
+        return this;
     }
 
-    private static String constructQuery(final Map<String,Object> values){
-        final StringBuilder OUT = new StringBuilder("");
+    final APICall withQuery(final String query, final Object value){
+        return withQuery(query, value);
+    }
 
-        values.forEach((k, v) -> {
-            // todo, skip null
-        });
+    final APICall withQuery(final String query, final Object value, final boolean encoded){
+        if(value == null)
+            queries.remove(query);
+        else
+            queries.put(query, encoded ? value.toString() : URLEncoder.encode(value.toString(), StandardCharsets.UTF_8));
+        return this;
+    }
 
-        return (OUT.length() > 0 ? "?" : "") + OUT.toString();
+    final APICall formUrlEncoded(){
+        return formUrlEncoded(true);
+    }
+
+    final APICall formUrlEncoded(final boolean formUrlEncoded){
+        this.formUrlEncoded = formUrlEncoded;
+        return this;
+    }
+
+    final APICall withField(final String field, final Object value){
+        return withField(field, value, false);
+    }
+
+    final APICall withField(final String field, final Object value, final boolean encoded){
+        if(value == null)
+            fields.remove(field);
+        else
+            fields.put(field, encoded ? value.toString() : URLEncoder.encode(value.toString(), StandardCharsets.UTF_8));
+        return this;
+    }
+
+    final Response<String> call() throws IOException{
+        final String URL =
+            baseURL +
+            pathArg.matcher(path).replaceAll(result -> pathVars.get(result.group())) + // path args
+            (queries.isEmpty() ? "" : queries.entrySet().stream().map(e -> e.getKey() + '=' +e.getValue()).collect( Collectors.joining("&"))); // query
+
+        final HttpURLConnection conn = (HttpURLConnection) new URL(URL).openConnection();
+        conn.setRequestMethod(method);
+        for(final Map.Entry<String, String> entry : headers.entrySet())
+            conn.setRequestProperty(entry.getKey(), entry.getValue());
+        conn.setUseCaches(false);
+        conn.setInstanceFollowRedirects(false);
+
+        if(formUrlEncoded){
+            final String data = fields.isEmpty() ? "" : fields.entrySet().stream().map(e -> e.getKey() + '=' +e.getValue()).collect( Collectors.joining("&"));
+            final byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("charset", "utf-8");
+            conn.setRequestProperty("Content-Length", Integer.toString(bytes.length));
+
+            try(final DataOutputStream OUT = new DataOutputStream(conn.getOutputStream())){
+                OUT.write(bytes);
+            }
+        }
+
+        final int rcode = conn.getResponseCode();
+
+        return new Response<>(
+            new BufferedReader(new InputStreamReader(rcode >= 200 && rcode < 300 ? conn.getInputStream() : conn.getErrorStream()))
+                .lines()
+                .collect(Collectors.joining("\n")),
+            rcode
+        );
+    }
+
+    final <T> Response<T> call(final Function<String,T> processor) throws IOException{
+        final Response<String> call = call();
+        return new Response<T>(processor.apply(call.response), call.code);
+    }
+
+    @Override
+    public String toString(){
+        return "APICall{" +
+               "method='" + method + '\'' +
+               ", baseURL='" + baseURL + '\'' +
+               ", headers=" + headers +
+               ", path='" + path + '\'' +
+               ", pathVars=" + pathVars +
+               ", queries=" + queries +
+               ", formUrlEncoded=" + formUrlEncoded +
+               ", fields=" + fields +
+               '}';
+    }
+
+    static final class Response<T>{
+
+        private final T response;
+        private final int code;
+
+        Response(final T response, final int code){
+            this.response = response;
+            this.code = code;
+        }
+
+        final T response(){
+            return response;
+        }
+
+        final int code(){
+            return code;
+        }
+
+        @Override
+        public String toString(){
+            return "Response{" +
+                   "response=" + response +
+                   ", code=" + code +
+                   '}';
+        }
+
     }
 
 }
