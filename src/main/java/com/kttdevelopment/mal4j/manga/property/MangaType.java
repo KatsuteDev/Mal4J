7a2b67fad1e97c73886da332a6d4e331e1566c20
Katsute <58778985+Katsute@users.noreply.github.com>
2021-03-04 20:28:07 -0500

    version

--- src/main/java/com/kttdevelopment/mal4j/manga/property/MangaType.java
+++ src/main/java/com/kttdevelopment/mal4j/manga/property/MangaType.java
@@ -23,7 +23,7 @@ package com.kttdevelopment.mal4j.manga.property;
  *
  * @see com.kttdevelopment.mal4j.manga.MangaPreview#getType()
  * @since 1.0.0
- * @version 1.0.0
+ * @version 1.1.0
  * @author Ktt Development
  */
 @SuppressWarnings("SpellCheckingInspection")
