cdad9f36436d70d3c587bc4814348e6a2e5f5cbf
Katsute <58778985+Katsute@users.noreply.github.com>
2021-04-10 20:43:03 -0400

    Fix Genre (#161)

---
+++ src/test/java/com/kttdevelopment/mal4j/TestGenre.java
@@ -0,0 +1,68 @@
+package com.kttdevelopment.mal4j;
+
+import com.kttdevelopment.mal4j.property.Genre;
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestGenre {
+
+    // \Qlabel_for_genre-\E(\d+)">([a-zA-Z]+)\Q</label>\E$
+    private static final Pattern label = Pattern.compile("\\Qlabel_for_genre-\\E(\\d+)\">([a-zA-Z]+)\\Q</label>\\E");
+
+    @BeforeAll
+    public static void beforeAll(){
+        Assumptions.assumeTrue(System.getProperty("java.version").charAt(0) != '9'); // SSL issue; OK to skip since other tests validate
+    }
+
+    @Test
+    public void testAnimeGenres() throws IOException{
+        final URL url = new URL("https://myanimelist.net/anime.php");
+
+        final BufferedReader IN = new BufferedReader(new InputStreamReader(url.openStream()));
+        final StringBuilder OUT = new StringBuilder();
+        String ln;
+        while((ln = IN.readLine()) != null)
+            OUT.append(ln);
+        IN.close();
+
+        final String raw = OUT.toString();
+
+        final Matcher matcher = label.matcher(raw);
+        while(matcher.find()){
+            Assertions.assertEquals(
+                Integer.parseInt(matcher.group(1)), // expected ID
+                Objects.requireNonNull(Genre.asEnum(matcher.group(2))).getAnimeGenreID(), //actual id
+                "MyAnimeList Anime Genre ID doesn't match Genre enum"
+            );
+        }
+    }
+
+    @Test
+    public void testMangaGenres() throws IOException{
+        final URL url = new URL("https://myanimelist.net/manga.php");
+
+        final BufferedReader IN = new BufferedReader(new InputStreamReader(url.openStream()));
+        final StringBuilder OUT = new StringBuilder();
+        String ln;
+        while((ln = IN.readLine()) != null)
+            OUT.append(ln);
+        IN.close();
+
+        final String raw = OUT.toString();
+
+        final Matcher matcher = label.matcher(raw);
+        while(matcher.find()){
+            Assertions.assertEquals(
+                Integer.parseInt(matcher.group(1)), // expected ID
+                Objects.requireNonNull(Genre.asEnum(matcher.group(2))).getMangaGenreID(), //actual id
+                "MyAnimeList Manga Genre ID doesn't match Genre enum"
+            );
+        }
+    }
+
+}
