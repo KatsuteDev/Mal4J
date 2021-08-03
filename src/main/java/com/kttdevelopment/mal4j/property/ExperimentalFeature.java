0d5a983d1da4cc661af0f326c00b7a326138ded1
Katsute <58778985+Katsute@users.noreply.github.com>
2021-08-02 20:50:54 -0400

    Experimental features should be enabled (#206)
    * add experimental features
    
    * fix (c)
    
    * should not have been deleted

---
+++ src/main/java/com/kttdevelopment/mal4j/property/ExperimentalFeature.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2021 Katsute
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.kttdevelopment.mal4j.property;
+
+import com.kttdevelopment.mal4j.anime.Anime;
+
+/**
+ * Represents experimental or undocumented features. If a feature is @deprecated it is no longer experimental, and you do not have to enable it.
+ *
+ * @see com.kttdevelopment.mal4j.MyAnimeList#enableExperimentalFeature(ExperimentalFeature)
+ * @since 2.3.0
+ * @version 2.3.0
+ * @author Katsute
+ */
+public enum ExperimentalFeature {
+
+    /**
+     * @see Anime#getOpeningThemes()
+     * @see Anime#getEndingThemes()
+     */
+    OP_ED_THEMES
+
+}
---
+++ src/test/java/com/kttdevelopment/mal4j/TestExperimentalEnabler.java
@@ -0,0 +1,30 @@
+package com.kttdevelopment.mal4j;
+
+import com.kttdevelopment.mal4j.property.ExperimentalFeature;
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestExperimentalEnabler {
+
+    private static MyAnimeList mal;
+
+    @BeforeAll
+    public static void beforeAll(){
+        mal = TestProvider.getMyAnimeList();
+    }
+
+    @Test @Order(0)
+    public void testExperimental(){
+        mal.getAnime(TestProvider.AnimeID).getOpeningThemes();
+    }
+
+    @Test @Order(1)
+    public void testExperimentalEnabled(){
+        mal.enableExperimentalFeature(ExperimentalFeature.OP_ED_THEMES);
+
+        mal.getAnime(TestProvider.AnimeID).getOpeningThemes();
+    }
+
+    // todo: add test cases for when an experimental feature becomes native
+
+}
