8ef88c01a3637d4b8449b735983132bdd262aff9
Katsute <58778985+Katsute@users.noreply.github.com>
2021-08-31 22:43:14 -0400

    (android) Use `X-HTTP-Method-Override` when PATCH is not available (#220)
    * PATCH for all!
    
    * Update AndroidCompatibilityException.java
    
    * switch to try catch on setRequestMethod (android may support patch)
    
    * Update APICall.java
    
    * Update APICall.java

--- src/main/java/com/kttdevelopment/mal4j/APICall.java
+++ src/main/java/com/kttdevelopment/mal4j/APICall.java
@@ -21,15 +21,14 @@ package com.kttdevelopment.mal4j;
 import java.io.*;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
 import java.lang.reflect.*;
-import java.net.HttpURLConnection;
-import java.net.URI;
+import java.net.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.*;
 import java.util.function.Function;
-import java.util.logging.Logger;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -43,11 +42,6 @@ import static com.kttdevelopment.mal4j.APIStruct.*;
 class APICall {
 
     static boolean debug = false;
-
-    @SuppressWarnings("CanBeFinal")
-    private static boolean PATCH_ENABLED = false;
-
-    private static final String PATCH_DISABLED = "The current Android version is not fully compatible with this library; PATCH methods will be disabled";
 
     private final String method;
     private final String baseURL;
@@ -177,30 +171,31 @@ class APICall {
 
     private static final boolean useNetHttp;
 
+    @SuppressWarnings("FieldCanBeLocal")
     private static class JDK11 {
 
-        static Class<?> HttpRequest;
-            static Method HttpRequest_NewBuilder;
+        private static Class<?> HttpRequest;
+            private static Method HttpRequest_NewBuilder;
 
-        static Class<?> HttpRequestBuilder;
-            static Method HttpRequestBuilder_URI;
-            static Method HttpRequestBuilder_Method;
-            static Method HttpRequestBuilder_Header;
-                static Method BodyPublishers_NoBody;
-                static Method BodyPublishers_StringBody;
-            static Method HttpRequestBuilder_Build;
+        private static Class<?> HttpRequestBuilder;
+            private static Method HttpRequestBuilder_URI;
+            private static Method HttpRequestBuilder_Method;
+            private static Method HttpRequestBuilder_Header;
+                private static Method BodyPublishers_NoBody;
+                private static Method BodyPublishers_StringBody;
+            private static Method HttpRequestBuilder_Build;
 
-        static Class<?> HttpClientBuilder;
-            static Method HttpClientBuilder_ConnectTimeout;
-            static Method HttpClientBuilder_Build;
+        private static Class<?> HttpClientBuilder;
+            private static Method HttpClientBuilder_ConnectTimeout;
+            private static Method HttpClientBuilder_Build;
 
-        static Class<?> HttpClient;
-            static Method HttpClient_NewBuilder;
-            static Method HttpClient_Send;
-                static Method BodyHandlers_StringBody;
+        private static Class<?> HttpClient;
+            private static Method HttpClient_NewBuilder;
+            private static Method HttpClient_Send;
+                private static Method BodyHandlers_StringBody;
 
-        static Method HttpResponse_Body;
-        static Method HttpResponse_Code;
+        private static Method HttpResponse_Body;
+        private static Method HttpResponse_Code;
 
         static {
             if(useNetHttp)
@@ -224,8 +219,6 @@ class APICall {
 
                     HttpResponse_Body = Class.forName("java.net.http.HttpResponse").getDeclaredMethod("body");
                     HttpResponse_Code = Class.forName("java.net.http.HttpResponse").getDeclaredMethod("statusCode");
-
-                    PATCH_ENABLED = true;
                 }catch(final ClassNotFoundException | NoSuchMethodException e){
                     throw new IllegalStateException(e);
                 }
@@ -233,7 +226,7 @@ class APICall {
 
     }
 
-    // initialize HTTPUrlConnection
+    // try to initialize HTTPUrlConnection
     static {
         final String version = System.getProperty("java.version");
         useNetHttp = (version != null ? Integer.parseInt(version.contains(".") ? version.substring(0, version.indexOf(".")) : version) : 0) >= 11;
@@ -255,9 +248,7 @@ class APICall {
                             try{ // Android API 1-8 (1 - 2.2.3)
                                 //noinspection JavaReflectionMemberAccess
                                 methods = HttpURLConnection.class.getDeclaredField("methodTokens");
-                            }catch(final NoSuchFieldException ignored3){
-                                Logger.getGlobal().warning(PATCH_DISABLED);
-                            }
+                            }catch(final NoSuchFieldException ignored3){ }
                         }
                     }
                 }
@@ -276,7 +267,6 @@ class APICall {
                                 modifiers = Class.forName("java.lang.reflect.ArtField").getDeclaredField("accessFlags");
                             }catch(final ClassNotFoundException | NoSuchFieldException ignored2){
                                 // Android API 1 (1.0) [NOT SUPPORTED]
-                                Logger.getGlobal().warning(PATCH_DISABLED);
                             }
                         }
                     }
@@ -295,21 +285,16 @@ class APICall {
                             newMethods.add("PATCH");
                             methods.set(null, newMethods.toArray(new String[0]));
 
-                            // set field to FINAL
+                            // reset field to FINAL
                             modifiers.setInt(methods, methods.getModifiers() | Modifier.FINAL);
                             methods.setAccessible(false);
                             modifiers.setAccessible(false);
-
-                            PATCH_ENABLED = true;
-                        }catch(final IllegalAccessException e){
-                            throw new IllegalStateException(e);
-                        }
+                        }catch(final IllegalAccessException ignored){ }
                     }
                 }
             }catch(final RuntimeException e){
-                throw e.getClass().getSimpleName().equals("InaccessibleObjectException")
-                    ? new IllegalStateException("Reflect module is not accessible in JDK 9+; add '--add-opens java.base/java.lang.reflect=Mal4J --add-opens java.base/java.net=Mal4J' to VM options, remove module-info.java, or compile the project in JDK 8 or JDK 11+")
-                    : e;
+                if(e.getClass().getSimpleName().equals("InaccessibleObjectException"))
+                    throw new IllegalStateException("Reflect module is not accessible in JDK 9+; add '--add-opens java.base/java.lang.reflect=Mal4J --add-opens java.base/java.net=Mal4J' to VM options, remove module-info.java, or compile the project in JDK 8 or JDK 11+");
             }
     }
 
@@ -417,9 +402,6 @@ class APICall {
                 throw new IllegalStateException(e);
             }
         else{
-            if(method.equalsIgnoreCase("PATCH") && !PATCH_ENABLED)
-                throw new AndroidCompatibilityException(PATCH_DISABLED);
-
             final HttpURLConnection conn = (HttpURLConnection) URI.create(Java9.Matcher.replaceAll(URL, blockedURI.matcher(URL), encoder)).toURL().openConnection();
 
             for(final Map.Entry<String, String> entry : headers.entrySet())
@@ -429,7 +411,15 @@ class APICall {
             conn.setRequestProperty("Accept", "application/json; charset=UTF-8");
             conn.setConnectTimeout(10_000);
             conn.setReadTimeout(10_000);
-            conn.setRequestMethod(method);
+
+            // set request method
+            try{
+                conn.setRequestMethod(method);
+            }catch(final ProtocolException ignored){
+                // MyAnimeList API supports `X-HTTP-Method-Override`
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("X-HTTP-Method-Override", "PATCH");
+            }
 
             if(formUrlEncoded){
                 conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
